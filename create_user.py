fbf102c95b66d5f62c11eea27a59d5fb0decab2e
tylersnel <snelgrove86@gmail.com>
2024-05-02 07:26:48 -0600

    Updates to create_user

--- create_user.py
+++ create_user.py
@@ -1,5 +1,17 @@
+
 import database
+
 object = database.DB()
-result = object.db_query("SELECT * FROM Employees")
-for i in result:
-    print(i)
+object.autocommit = True
+
+#object.db_query("CREATE TABLE IF NOT EXISTS Employees (id INT PRIMARY KEY, username VARCHAR(255), password VARCHAR(255))")
+
+def create_user():
+    user_name=input("Please enter username: ")
+    password=input("Please enter password: ")
+    query= "INSERT INTO Employees (username, password) VALUES (%s, %s)"
+
+    if object.db_insert_new_user(query, user_name, password):
+        print("User Succesfully Added")
+
+create_user()
