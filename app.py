9b6f81aab18069e2811f1d98abd758b07fbd0635
tylersnel <snelgrove86@gmail.com>
2024-08-22 07:30:30 -0600

    Delete unused .py files. Working on refactoring

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 import flask
-from flask import Flask, render_template, request, redirect, url_for, session, flash, send_file
+from flask import render_template, request, redirect, url_for, session, flash, send_file
 import os
 import database
 from key import key
 import io
-import mimetypes
 
 object = database.DB()
 app = flask.Flask(__name__)
 app.secret_key = key
 
-#function used to allow for easier redirection back to home page for employees when coming from other pages
+
 def render_employee_home(user_name, tech_id):
+    #function used to allow for easier redirection back to home page for employees when coming from other pages
     query2 = "SELECT * from Actions WHERE assigned_tech_id is NULL"
     unassigned_actions = object.db_query(query2)
     action_status='Assigned'
@@ -20,28 +20,24 @@ def render_employee_home(user_name, tech_id):
     assigned_actions =  object.db_signin(query3, tech_id, action_status)
     return render_template("employee_home.j2", user_name=user_name, tech_id=tech_id, unassigned_actions=unassigned_actions, assigned_actions=assigned_actions)   
 
-def render_customer_home(user_name, customer_id):
-    #Getting all assigned actions created by current user
+def render_customer_home(customer_id):
+    #function used to allow for easier redirection back to home page for customerswhen coming from other pages
+    #Uses db_signin funtion to reduce need of multiple functions
+    
+    #Gets actions created by current user
     action_status_assigned='Assigned'
     query = "SELECT * from actions WHERE action_creator= %s AND action_status =%s"
-    #using the db_signin in fuction so not to have duplicate queries
     assigned_actions = object.db_signin(query, customer_id, action_status_assigned)
     
-    #Getting all unassigned actions created by current user
     action_status_unassigned='unassigned'
-    query2 = "SELECT * from actions WHERE action_creator= %s AND action_status =%s"
-    #using the db_signin in fuction so not to have duplicate queries
-    unassigned_actions = object.db_signin(query2, customer_id, action_status_unassigned)
+    unassigned_actions = object.db_signin(query, customer_id, action_status_unassigned)
 
-    #Getting all RWO actions created by current user
-    action_status_RWO='RWO'
-    query3 = "SELECT * from actions WHERE action_creator= %s AND action_status =%s"
-    #using the db_signin in fuction so not to have duplicate queries
-    RWO_actions = object.db_signin(query3, customer_id,  action_status_RWO)
+    action_status_rwo='RWO'
+    rwo_actions = object.db_signin(query, customer_id,  action_status_rwo)
 
-    return render_template("customer_home.j2", assigned_actions=assigned_actions, unassigned_actions=unassigned_actions, RWO_actions=RWO_actions)
+    return render_template("customer_home.j2", assigned_actions=assigned_actions, unassigned_actions=unassigned_actions, RWO_actions=rwo_actions)
 
-#helper function to send variables to render home pages. Also helps with redirection back to home page  when coming from other pages
+#helper function to send variables to render home pages. Also helps with redirection back to home page when coming from other pages
 @app.route('/', methods=["GET"])
 def main():
     if 'username' in session:
@@ -51,34 +47,38 @@ def main():
             tech_id=session['id']
             return render_employee_home(user_name, tech_id)
         elif permissions==1: #if customer
-            user_name=session['username']
             customer_id=session['id']
-            return render_customer_home(user_name, customer_id)
+            return render_customer_home(customer_id)
     return render_template("login.j2")
 
 @app.route('/login', methods=["GET"])
 def login():
     if request.method == "GET":
-        user_name=request.args.get("user_name")
-        password=request.args.get("password")
+        user_name = request.args.get("user_name")
+        password = request.args.get("password")
 
-        query= "SELECT * from Employees WHERE username = %s AND password = %s"
-
+        # Using parameterized query to prevent SQL injection
+        query = "SELECT * FROM Employees WHERE username = %s AND password = %s"
         user = object.db_signin(query, user_name, password)
-        #for failed login attempt
+
+        # For failed login attempt
         if not user:            
             return render_template("login.j2", login_error=True)
 
-        session['username']=user_name
-        session['id']=user[0][0]
-        session['permissions']=user[0][3]
-        return redirect (url_for('main'))
-        
+        session['username'] = user_name
+        session['id'] = user[0][0]
+        session['permissions'] = user[0][3]
+        return redirect(url_for('main'))
 
 
 @app.route('/edit_action/<int:action_id>', methods=["POST", "GET"])
 def edit_action(action_id):
     if request.method == "GET":
+        return handle_edit_get(action_id)
+    elif request.method == "POST":
+        return handle_edit_post(action_id)
+
+def handle_edit_get(action_id):
         query = "SELECT * from Actions WHERE action_id = %s"
         action = object.db_action_search(query, action_id)
         query2 = "SELECT * from Comments WHERE action_id = %s"
@@ -86,81 +86,82 @@ def edit_action(action_id):
         query3 = "SELECT * from Files WHERE action_id = %s"
         files = object.db_action_search(query3, action_id)
         return render_template("edit_action.j2", action=action, comments=comments, files=files)
-    elif request.method == "POST":
-        #if the edit button is clicked
+
+def handle_file(action_id):
+    files = request.files.getlist('files')
+    for file in files:
+         if file:
+            file_data = file.read()
+            file_name = file.filename
+            mime_type = file.mimetype
+            action_creator=session['id']
+            query4 = "INSERT INTO files (action_id, file_name, file_data, mime_type, user_id) VALUES (%s, %s, %s, %s, %s)"
+            object.db_upload_files(query4, action_id, file_name, file_data, mime_type, action_creator)
+
+def handle_edit_post(action_id):
         if request.form.get("Edit_Action"):
             a_id = request.form["action_id"]
             unit = request.form["unit"]
             a_type = request.form["action_type"]
             last_name = request.form["sm_last_name"]
             tech_id = request.form["assigned_tech_id"]
-            if not tech_id: # if there is no tech assinged yet. None allows the querey to execute because is FK
+            if not tech_id: # if there is no tech assinged yet. None allows the querey to execute because of FK
                 tech_id = None
             status = request.form["action_status"]
             new_comment = request.form["comment"]
             if status == "Unassigned" or status == "RWO":
-                tech_id = None #also unassigning tech_id
+                tech_id = None #also unassigning tech_id if action is being returned so other techs can see it when action comes back
             query2 = "UPDATE actions SET unit_name = %s, action_type = %s, sm_last_name = %s, assigned_tech_id = %s, action_status = %s WHERE action_id = %s"
             object.db_action_edit(query2, unit, a_type, last_name, tech_id, status, a_id)
             if new_comment !='':
-                commentor_id=session['id'] # ID of who is making the comment
+                commentor_id=session['id'] 
                 query3 = "INSERT INTO comments (comment, action_id, user_id) VALUES (%s, %s, %s)"
                 object.db_assign_action(query3, new_comment,a_id,commentor_id)
             
             if 'files' in request.files:
-                files = request.files.getlist('files')
-                for file in files:
-                    if file:
-                        file_data = file.read()
-                        file_name = file.filename
-                        mime_type = file.mimetype
-                        action_creator=session['id']
-                        query4 = "INSERT INTO files (action_id, file_name, file_data, mime_type, user_id) VALUES (%s, %s, %s, %s, %s)"
-                        object.db_upload_files(query4, action_id, file_name, file_data, mime_type, action_creator)
+                handle_file(action_id)
 
             return redirect (url_for('main'))
         
 @app.route('/assign_action/<int:tech_id>/<int:action_id>', methods=["POST", "GET"])
 def assign_action(tech_id, action_id):
-    if request.method == "POST":
-        if request.form.get("assign"):
-            action_status= "Assigned"
-            query = "UPDATE actions SET assigned_tech_id = %s, action_status = %s  WHERE action_id = %s"
-            result = object.db_assign_action(query, tech_id, action_status, action_id)
+    if request.method == "POST" and request.form.get("assign"):
+        action_status= "Assigned"
+        query = "UPDATE actions SET assigned_tech_id = %s, action_status = %s  WHERE action_id = %s"
+        result = object.db_assign_action(query, tech_id, action_status, action_id)
         
-            if result:
-                    return redirect (url_for('main'))
-            else:
-                print("Failed")
-                return render_template("login.j2")
+        if result:
+            return redirect (url_for('main'))
+        else:
+            print("Failed")
+            return render_template("login.j2")
 
 @app.route('/create_action', methods=["POST", "GET"])
 def create_action():
-    if request.method == "POST":
-        if request.form.get("Create_Action"):
-            unit = request.form["unit"]
-            action_type = request.form["action_type"]
-            sm_last_name = request.form["sm_last_name"]
-            action_status = "Unassigned"
-            action_creator = session['id']
-            comment= request.form["comments"]
-            query = "INSERT INTO actions (unit_name, action_type, sm_last_name, action_status, action_creator) VALUES (%s, %s, %s, %s, %s)"
-            query2 = "INSERT INTO comments(comment, action_id, user_id) VALUES (%s, %s, %s)"
-            action_id = object.db_create_ticket(query, query2, unit, action_type, sm_last_name, action_status, action_creator, comment)
+    if request.method == "POST" and request.form.get("Create_Action"):
+        unit = request.form["unit"]
+        action_type = request.form["action_type"]
+        sm_last_name = request.form["sm_last_name"]
+        action_status = "Unassigned"
+        action_creator = session['id']
+        comment= request.form["comments"]
+        query = "INSERT INTO actions (unit_name, action_type, sm_last_name, action_status, action_creator) VALUES (%s, %s, %s, %s, %s)"
+        query2 = "INSERT INTO comments(comment, action_id, user_id) VALUES (%s, %s, %s)"
+        action_id = object.db_create_ticket(query, query2, unit, action_type, sm_last_name, action_status, action_creator, comment)
             
-            if 'files' in request.files:
-                files = request.files.getlist('files')
-                for file in files:
-                    if file:
-                        file_data = file.read()
-                        file_name = file.filename
-                        mime_type = file.mimetype
-                        query3 = "INSERT INTO files (action_id, file_name, file_data, mime_type, user_id) VALUES (%s, %s, %s, %s, %s)"
-                        object.db_upload_files(query3, action_id, file_name, file_data, mime_type, action_creator)
-                flash("Action Created")        
-                return redirect (url_for('main'))
-            flash("Action Created")    
+        if 'files' in request.files:
+            files = request.files.getlist('files')
+            for file in files:
+                if file:
+                    file_data = file.read()
+                    file_name = file.filename
+                    mime_type = file.mimetype
+                    query3 = "INSERT INTO files (action_id, file_name, file_data, mime_type, user_id) VALUES (%s, %s, %s, %s, %s)"
+                    object.db_upload_files(query3, action_id, file_name, file_data, mime_type, action_creator)
+            flash("Action Created")        
             return redirect (url_for('main'))
+        flash("Action Created")    
+        return redirect (url_for('main'))
 
     return render_template("create_action.j2")
 
